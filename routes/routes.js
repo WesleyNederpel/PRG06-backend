ee8dda8846395f676b4e28caede1afa57eb8e1d6
wesle <wesleynederpel43@gmail.com>
2025-01-21 10:06:44 +0100

    GET collection ADD seed v1

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
-import Set from "../models/LegoSet";
+import Set from "../models/LegoSet.js";
+import {faker} from "@faker-js/faker"
 import express from "express"
 
 const router = express.Router()
@@ -19,6 +20,29 @@ router.get('/', async (req, res) => {
             }
         }
         res.status(200).json(collection)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+})
+
+router.post('/seed', async (req, res) => {
+    try {
+        await Set.deleteMany({})
+
+        const amount = req.body.amount
+
+        console.log(amount)
+
+        for (let i = 0; i < amount; i++) {
+            await Set.create({
+                name: faker.lorem.lines(1),
+                brand: faker.lorem.lines(1),
+                setNumber: faker.helpers.uniqueArray(1),
+                releaseYear: faker.date.past(3)
+            });
+        }
+
+        res.status(200).json({success: true})
     } catch (error) {
         res.status(500).json({error: error.message})
     }
